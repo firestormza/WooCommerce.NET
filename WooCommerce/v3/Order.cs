a4fe802e9e51161d77b9ede38fd4197305078373
firestormza <13041604+firestormza@users.noreply.github.com>
2024-09-19 03:16:22 +0200

    Add v3.Order.pw_gift_cards_redeemed list of Pimwick PW Gift Card Plugin

--- WooCommerce/v3/Order.cs
+++ WooCommerce/v3/Order.cs
@@ -8,6 +8,33 @@ namespace WooCommerceNET.WooCommerce.v3
     public class OrderBatch : BatchObject<Order> { }
 
     [DataContract]
-    public class Order : v2.Order { }
-    
+    public class Order : v2.Order 
+    {
+        /// <summary>
+        /// List of Pw Gift Card Redeemed Lines. See Order - Refunds properties 
+        /// read-only
+        /// </summary>
+        /// CFC Added
+        [DataMember( EmitDefaultValue = false )]
+        public List<PwGiftCardRedeemedLine> pw_gift_cards_redeemed { get; set; }
+    }
+
+    /// CFC Added
+    [DataContract]
+    public class PwGiftCardRedeemedLine: JsonObject
+    {
+        /// <summary>
+        /// Gift Card Number 
+        /// </summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string number { get; set; }
+
+        /// <summary>
+        /// Amount as a string
+        /// </summary>
+        [DataMember( EmitDefaultValue = false )]
+        public string amount { get; set; }
+    }
+
+
 }
